1b1dfef6b3c7648e03b64b74c409bf1f00f82318
zhangqiang <zhangqiang@actionsky.com>
2021-04-13 10:18:01 +0800

    Update

--- https-demo/https-client-demo/main.go
+++ https-demo/https-client-demo/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	resp, err := http.Get("http://localhost/hello")
+	resp, err := http.Get("https://127.0.0.1:80/hello")
 	if err != nil {
 		fmt.Println(err)
 	}
--- https-demo/https-server-demo/main.go
+++ https-demo/https-server-demo/main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	// "fmt"
-	// "io"
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte("This is an example server.\n"))
-	// fmt.Fprintf(w, "This is an example server.\n")
-	// io.WriteString(w, "This is an example server.\n")
 }
 
 func main() {
 	http.HandleFunc("/hello", HelloServer)
-	fmt.Println("Prepare Listen 433")
-	err := http.ListenAndServeTLS(":80", "server.crt", "server.key", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	fmt.Println("Prepare Listen 80")
+	http.ListenAndServeTLS(":80", "server.crt", "server.key", nil)
 }
